37781571a7e48bcdea3aa5b9827fd53cc8718e12
luubinhan <luubinhan1989@gmail.com>
2026-07-04 11:15:43 +0700

    Add grade image and update CoursePage background: Introduce a new grade image and modify the CoursePage component to use the new background image for enhanced visual appeal.

--- src/pages/CoursePage.tsx
+++ src/pages/CoursePage.tsx
@@ -1,4 +1,5 @@
 import { courseProfile, courseUnits } from "../data/course";
+import mountainsBg from "../a.jpg";
 import {
   CourseBottomNav,
   CoursePageTitle,
@@ -7,7 +8,7 @@ import {
 
 export default function CoursePage() {
   return (
-    <div className="relative min-h-screen bg-gradient-to-b from-sky-50 via-sky-50 to-blue-100/80 pb-24">
+    <div className="relative min-h-screen bg-gradient-to-b from-sky-50 via-sky-50 to-blue-100/80 bg-center bg-top bg-[url(/map/grade1.png)] pb-24">
       <div className="mx-auto max-w-lg">
         <CoursePageTitle curriculumLabel={courseProfile.curriculumLabel} />
         <LearningPath units={courseUnits} />
