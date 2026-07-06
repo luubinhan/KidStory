052e1b195000a400a2539d0dd82b9c8d612efefd
luubinhan <luubinhan1989@gmail.com>
2026-07-06 18:24:20 +0700

    feat: enhance GameQuestionImage component with dynamic background styling
    - Updated the GameQuestionImage component to utilize a mask SVG and dynamic background image styling, improving visual presentation.
    - Adjusted CSS classes for better responsiveness and visual consistency, while hiding the img element for a cleaner design.

--- src/components/game-topic/GameQuestionImage.tsx
+++ src/components/game-topic/GameQuestionImage.tsx
@@ -4,11 +4,18 @@ type GameQuestionImageProps = {
 
 export function GameQuestionImage({ src }: GameQuestionImageProps) {
   return (
-    <div className="mt-4 aspect-[7/4] w-full overflow-hidden mb-5 flex items-center justify-center border-12 border-white rounded-xl bg-white">
+    <div className="mt-4 aspect-[7/4] w-full mask-[url('https://luubinhan.github.io/KidStory/images/mask.svg')] size-64 overflow-hidden mb-5 flex items-center justify-center rounded-xl backdrop-brightness-90"
+      style={{
+        backgroundImage: `url(${src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <img
         src={src}
         alt=""
-        className="rounded-xl object-cover size-64 "
+        className="rounded-xl object-cover hidden  size-64 "
         referrerPolicy="no-referrer"
       />
     </div>
